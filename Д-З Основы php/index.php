588bee069e4ac45f87ac4a9e99364a62b5c9c58a
Belena27089 <bistrova71@yandex.ru>
2016-12-31 02:51:02 +0700

    Убраны лишние выводы ошибок

--- Д-З Основы php/index.php
+++ Д-З Основы php/index.php
@@ -1,20 +1,20 @@
 
 <?php
 
-error_reporting(E_ALL|E_ERROR|E_COMPILE_WARNING|E_PARSE|E_USER_NOTICE);
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $name = 'Елена';
 $age = 45;
 
 echo 'Меня зовут ' . $name . ' .<br>';
-echo 'Мне ' .$age . ' лет.';
+echo 'Мне ' . $age . ' лет.';
 
 //удаление переменных
-unset($name ,$age );
+unset($name, $age);
 
 echo 'Меня зовут ' . $name . ' .<br>';
-echo 'Мне ' .$age . ' лет.<br>';
+echo 'Мне ' . $age . ' лет.<br>';
 
 //создание константы
 define('CITY', 'Новосибирск');
@@ -22,34 +22,29 @@ define('CITY', 'Новосибирск');
 //проверка наличия константы
 //print_r(get_defined_constants());
 if (defined('CITY')) {
-    echo 'константа существует'. '<br>';
+    echo 'константа существует' . '<br>';
+} else {
+    echo 'константы не существует' . '<br>';
 }
- else {
-    echo 'константы не существует'. '<br>';
-}
-echo 'Город: ' . CITY. '<br>';
+echo 'Город: ' . CITY . '<br>';
 
 //изменение значения константы
 define('CITY', 'Москва');
-echo 'Город: ' . CITY. '<br>';
+echo 'Город: ' . CITY . '<br>';
 
 //создание ассоциативного массива
 
-$book=array('title'=>'"Наследники богов"','author'=>'Рик Риордан','pages'=>'300');
+$book = array('title' => '"Наследники богов"', 'author' => 'Рик Риордан', 'pages' => '300');
 
-echo 'Недавно я прочитала книгу  '. $book['title'] . ',написанную автором  '.$book['author'] .', я осилила '.$book['pages'].' страниц, мне она очень понравилась.<br>';
+echo 'Недавно я прочитала книгу  ' . $book['title'] . ',написанную автором  ' . $book['author'] . ', я осилила ' . $book['pages'] . ' страниц, мне она очень понравилась.<br>';
 
 //создание индексного массива с вложенными ассоциативными массивами
 
+$book1 = ['title' => '"Наследники богов"', 'author' => 'Рик Риордан', 'pages' => '300'];
+$book2 = ['title' => '"Два капитана"', 'author' => 'Вениамин Каверин', 'pages' => '200'];
+$books = array($book1, $book2);
 
-$books = array(
-    $book1=['title'=>'"Наследники богов"','author'=>'Рик Риордан','pages'=>'300'],
-    $book2=['title'=>'"Два капитана"','author'=>'Вениамин Каверин','pages'=>'200']
-    );
-    
-echo 'Недавно я прочитала книги  '. $books[0]['title'].' и '.$books[1]['title']. ', написанную автороми  '.$books[0]['author'].' и '.$books[1]['author'].', я осилила в сумме '.($books[0]['pages']+$books[1]['pages']).' страниц, не ожидала от себя такого.';
-
-    
+echo 'Недавно я прочитала книги  ' . $books[0]['title'] . ' и ' . $books[1]['title'] . ', написанную автороми  ' . $books[0]['author'] . ' и ' . $books[1]['author'] . ', я осилила в сумме ' . ($books[0]['pages'] + $books[1]['pages']) . ' страниц, не ожидала от себя такого.';
 ?>
 
 
